ead1118f7a17c978c3dca1bbcb74bddd089b62f9
Dominika Boron <borond@mcmaster.ca>
2021-02-27 13:59:32 -0500

    assignment 4 no sourcing

--- assignment_4.R
+++ assignment_4.R
@@ -1,4 +1,4 @@
-#Assignment 4 - computing permutation tests
+# Assignment 4 - computing permutation tests
 
 # Setup ------------------------------------------------------------------------
 
@@ -22,8 +22,98 @@ asvtab = read.csv("asvtab.csv") #asv frequency table
 taxtab = read.csv("taxatab.csv") #taxonomy file
 mapfile= read.csv("updated_samplemap.csv") #sample information file
 
-source("cleandata.R")
-dat_rel <- readRDS("dat_rel.rds")
+#Format tables ----
+
+#convert the rownames of your sample file to DBIDs. 
+#These are the fastq IDs so they are essential to use here
+
+rownames(mapfile) = mapfile$WPNum
+rownames(mapfile)
+colnames(mapfile) 
+
+#convert the asv sequences to the rownames for both the asvtab and taxtab
+#this is important because we need to have matching rownames to convert these to 
+#phyloseq objects. 
+
+rownames(asvtab) = asvtab$ASV
+rownames(taxtab) = taxtab$X
+head(taxtab)
+
+#checks if the rownames in samdat match the colnames of asvtab. prints a logical response
+#next line will print out the sample IDs that do not match
+
+all(rownames(mapfile) %in% colnames(asvtab))
+rownames(mapfile)[!(rownames(mapfile) %in% colnames(asvtab))]
+
+rs <- rownames(mapfile)
+bad <- which(!rs %in% colnames(asvtab))
+if (length(bad)>0) {
+  stop("non-matching rownames: ",
+       paste(rs[bad], collapse=", "))
+}
+
+rownames(mapfile)
+colnames(asvtab) 
+
+#checks if the colnames in asvtab match the rownames of the sample file. logical response
+#next line will print out the sample IDs that do not match
+
+all(colnames(asvtab) %in% rownames(mapfile))
+colnames(asvtab)[!(colnames(asvtab) %in% rownames(mapfile))]
+
+rs <- colnames(mapfile)
+bad <- which(!rs %in% colnames(asvtab))
+if (length(bad)>0) {
+  stop("non-matching colnames: ",
+       paste(rs[bad], collapse=", "))
+}
+
+#Convert to matrices -----------------------------------------------------------
+#To use this data in a phyloseq object we must convert our dataframes 
+#to matrices. The asvtab must be an integer, and the taxtab must be character. 
+
+#converting data frame to integer matrix, use dim(asvtab)
+dim(asvtab)
+asv <- asvtab[,2:274] 
+asvtable <- data.matrix(asv,rownames.force = NA)
+class(asvtable)
+str(asvtable) #make sure asvtab is integers and characters, checks the structure of the data
+
+#converting dataframe into character matrix
+dim(taxtab) #to determine how to truncate
+tax <- taxtab[,-1] #remove first column
+
+taxtable <- as.matrix(tax,rownames.force = NA)
+class(taxtable) #make sure it is a matrix at this point
+stopifnot(inherits(taxtable,"matrix"))
+str(taxtable) #make sure output is all characters
+
+#Making the phyloseq object ----------------------------------------------------
+
+dat = phyloseq(otu_table(asvtable, taxa_are_rows = TRUE),
+               tax_table(taxtable),
+               sample_data(mapfile))
+dat
+
+#check sample sums
+summary(sample_sums(dat))
+sample_sums(dat)
+
+
+sample_data(dat)[sample_sums(dat) < 2500,]
+
+#filter out mitochondria bacteria from host
+dat = subset_taxa(dat, Kingdom=="Bacteria", Family!="Mitochondria")
+
+samdat_clean = data.frame(sample_data(dat)) #make a dataframe
+
+#transform asv counts into relative abundance data (i.e. calculate relative abundance) 
+
+dat_rel = transform_sample_counts(dat, function(x) x/sum(x)) 
+
+#subset data into nasal and oral samples
+dat.nasal = subset_samples(dat_rel, Sample.Type=='Nasal')
+dat.sal = subset_samples(dat_rel, Sample.Type=='Saliva')
 
 # testing compositional differences between sample types
 
@@ -49,11 +139,24 @@ permutest(dispr_s)
 alpha_div <- microbiome::alpha(dat_rel, index = "diversity_shannon")
 print(alpha_div)
 
+as.data.frame(mapfile)
+
 #brute force permutation test
-set.seed(101)
+set.seed(101) ##for reproducibility
 nsim <- 9999
-res <- numeric(nsim)
+res <- numeric(nsim) ##set aside space for results
 for (i in 1:nsim) {
-  perm <- sample(nrow(alpha_div))
-  bdat <- transform(alpha_div,diversity_shannon=diversity_shannon[perm])
-  res[i] <- mean(bdat$alpha_div[bdat$diversity_shannon])}
+  ## standard approach: scramble response value
+  perm <- sample(nrow(mapfile))
+  bdat <- transform(mapfile,Age=Age[perm])
+  ## compute & store difference in means; store the value
+  res[i] <- mean(bdat$mapfile[bdat$Time=="Baseline"])-
+    mean(bdat$mapfile[bdat$Time=="FollowUp"])
+}
+obs <- mean(mapfile$Age[mapfile$Time=="Baseline"])-
+  mean(mapfile$Age[mapfile$Time=="FollowUp"])
+## append the observed value to the list of results
+res <- c(res,obs)
+
+hist(res,col="grey",las=1, main="")
+abline(v=obs,col="red")
